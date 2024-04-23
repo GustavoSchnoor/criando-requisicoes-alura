58dee8f70f1ff5d71cb4937d21a89d65d3949311
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-04-23 13:24:40 -0300

    Att filtro de busca e exclusão das info antiga da lista

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,11 +1,18 @@
 import { conectaApi } from './conectaApi.js';
+import constroiCard from './mostrarVideos.js';
 
 async function buscarVideo(evento) {
     evento.preventDefault();
     const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
     const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-    console.log(busca);
+    const lista = document.querySelector('[data-lista]');
+    while(lista.firstChild) {
+        lista.removeChild(lista.firstChild); // PARA REMOVER SEMPRE O PRIMEIRO FILHO
+    }
+    // lista.innerHTML = ''     OU PODERIA RESOLVER ASSIM TAMBEM PARA LIMPAR TUDO O QUE TEM DENTRO DA LISTA
+    
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
 
 const botaoDePesquisa = document.querySelector('[data-botao-pesquisa]');
